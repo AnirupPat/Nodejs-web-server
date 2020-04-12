1b4841c852e8fb4f2b265280b191ce1081571e56
Anirup Patnaik <anirup049@gmail.com>
2020-04-12 08:58:33 +0530

    Serving up static content

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require('path') // we dont need to install it as its a core node module.. 
 const express = require('express')
 // express is actually a function, and we call express to create a new express app
 // To generate the app we need to call express as below
@@ -6,25 +7,26 @@ const app = express()
 // app.com
 // app.com/help
 // app.com/about
+const publicDir = path.join(__dirname,'..', '/public')
+app.use(express.static(publicDir))
 
+// let these below code be commented
 // To set up the server to send a response, by using the get method
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name: 'Anirup',
+//         age: 29
+//     },{
+//         name: 'Sarah',
+//         age: 29
+//     }])
+// })
 
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Anirup',
-        age: 29
-    },{
-        name: 'Sarah',
-        age: 29
-    }])
-})
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About Us</h1>')
+// })
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About Us</h1>')
-})
+
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -39,3 +41,9 @@ app.listen(3000, () => {
 }) // this starts up the server and it will run in a specific port
 
 // the process of starting a server is an asynchronous process
+
+console.log(__dirname)
+console.log(__filename)
+
+
+console.log(path.join(__dirname,'..', '/public'))
